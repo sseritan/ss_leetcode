c1405c8a4a2b4e8c39c0e42cbb4a165f6ff1912f
sseritan <sseritan@gmail.com>
2020-05-23 23:18:19 -0700

    Faster 287 using Floyd's cycle detection

--- 0287_DupNum/src/main.rs
+++ 0287_DupNum/src/main.rs
@@ -1,22 +1,26 @@
 struct Solution;
 impl Solution {
     // Extra stipulations: Use read-only array, O(1) space, and no worse than O(N^2) time
-    // First attempt: Run through array, looking for each number
+    // Second attempt: Floyd's Tortoise and Hare
     pub fn find_duplicate(nums: Vec<i32>) -> i32 {
-        for target in 1..nums.len() {
-            let mut count = 0;
-            for n in &nums {
-                if target as i32 == *n {
-                    count += 1;
-                }
-            }
+        let mut slow = nums[nums[0] as usize];
+        let mut fast = nums[nums[nums[0] as usize] as usize];
 
-            if count > 1 {
-                return target as i32
-            }
+        // Phase 1: Hare moves at double speed until it meets Tortoise
+        while slow != fast {
+            slow = nums[slow as usize];
+            fast = nums[nums[fast as usize] as usize];
         }
 
-        -1 // Dummy return
+        // Phase 2: Tortoise restarts and Hare moves at slow speed
+        // They should meet at entrance to cycle, which is duplicate
+        slow = nums[0];
+        while slow != fast {
+            slow = nums[slow as usize];
+            fast = nums[fast as usize];
+        }
+
+        slow
     }
 }
 
