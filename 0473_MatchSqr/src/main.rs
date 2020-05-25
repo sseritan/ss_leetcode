7927c41a9cdbe945dd54c524950bd8f787f30e0b
sseritan <sseritan@gmail.com>
2020-05-24 17:56:45 -0700

    DP subsum finding for 0473
    Working to find one subsum, but still not choosing optimal for 5,4,3
    case.

--- 0473_MatchSqr/src/main.rs
+++ 0473_MatchSqr/src/main.rs
@@ -1,5 +1,48 @@
+use std::collections::HashMap;
+
 struct Solution;
 impl Solution {
+    // For each side, need to find entries that sum up to a side
+    // If I knew it was exactly two entries, could do the complement trick from 0001
+    // I just need to find ONE subsum, so can try and adapt https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+    fn findsubsum(nums: &[i32], target: i32) -> Option<Vec<usize>> {
+        // Key is subsum, value is list of indices to build key
+        let mut subset: HashMap<i32, Vec<usize>> = HashMap::new();
+        subset.insert(0, vec![]); // Base case
+
+        for i in 0..nums.len() {
+            let val = nums[i];
+
+            for subsum in 1..target+1 {
+                let subsum = subsum as i32;
+
+                if subset.contains_key(&subsum) {
+                    //println!("Index: {} (val: {}) subsum: {} subset: {:?}... skipping", i, val, subsum, subset);
+                    continue;
+                }
+
+                let remainder = subsum - val;
+                if let Some(ssindex) = subset.get(&remainder) {
+                    if !ssindex.contains(&i) { // Avoid repeats
+                        let mut index = ssindex.clone();
+                        index.push(i);
+
+                        if subsum == target {
+                            //println!("Index: {} (val: {}) subsum: {} subset: {:?}... returning", i, val, subsum, subset);
+                            return Some(index)
+                        } else {
+                            subset.insert(subsum, index);    
+                        }
+                    }
+                } // end if let Some(ssindex)
+
+                //println!("Index: {} (val: {}) subsum: {} subset: {:?}", i, val, subsum, subset);
+            } // end for subsum
+        } // end for i
+
+        subset.remove(&target)
+    } // end fn findsubsum
+
     pub fn makesquare(mut nums: Vec<i32>) -> bool {
         let length: i32 = (nums.iter().sum::<i32>()) / 4;
 
@@ -8,32 +51,17 @@ impl Solution {
             return false
         }
 
-        // O(NlogN) sort in descending
-        nums.sort_unstable_by(|a, b| b.cmp(a));
-
-        for _side in 0..4 {
-            println!("Searching for side {}", _side);
-            let mut indices = vec![];
-            let mut remainder = length;
-
-            for (i,num) in nums.iter().enumerate() {
-                if *num <= remainder {
-                    remainder -= *num;
-                    indices.insert(0, i); // Prepend to avoid index issues in remove
+        for side in 0..4 {
+            match Solution::findsubsum(&nums[..], length) {
+                None => {
+                    return false
+                },
+                Some(indices) => {
+                    println!("Found {:?} for side {}", indices, side);
+                    for i in indices.iter().rev() {
+                        nums.remove(*i);
+                    }                    
                 }
-                if remainder == 0 {
-                    break;
-                }
-            }
-
-            if remainder != 0 {
-                // Could not make a side of right length
-                return false
-            }
-
-            println!("Found side: {:?}", indices);
-            for i in indices {
-                nums.remove(i);
             }
         }
 
